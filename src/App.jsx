05f26838f8ca5e4c968b83301b278593d194725a
Filip Kulic <filip.kulic.dev@outlook.com>
2025-06-18 14:42:50 +0200

    Import font and add default styles

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './public /react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
